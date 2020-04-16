fe65af013594647907c94fa58bf76e03a33aade7
alexUIForTests <62977593+alexUIForTests@users.noreply.github.com>
2020-04-16 18:59:54 +0300

    add envir for build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firabaseConfig: {
+    apiKey: 'AIzaSyByu_tdT3CQ1XNv9nqfb2mpzuUJSf90Gbo',
+    authDomain: 'starwars-428c7.firebaseapp.com',
+    databaseURL: 'https://starwars-428c7.firebaseio.com',
+    projectId: 'starwars-428c7',
+    storageBucket: 'starwars-428c7.appspot.com',
+    messagingSenderId: '1043423661262',
+    appId: '1:1043423661262:web:7493568a8cdef43c3b949f'
+  }
 };
